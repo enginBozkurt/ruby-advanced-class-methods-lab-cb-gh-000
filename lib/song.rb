f9fd5f548e227ba4881f5ce74d08cbbfafdee36f
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-29 12:37:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,13 @@ class Song
    song
  end
 
-
+ def self.find_or_create_by_name(name)
+     if !self.find_by_name(name).nil?
+       self.find_by_name(name)
+     else
+       self.create_by_name(name)
+     end
+   end
 
 
 
